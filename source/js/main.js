ac510c773065b54873aafa114511d16477077777
Fedor Medvedev <fedmed25@yandex.ru>
2021-04-24 16:41:04 +0700

    Изменён скрипт для навигационного меню в мобильной версии

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,7 @@
 let buttonNav = document.querySelector('.main-navigation__toggle');
 let nav = document.querySelector('.main-navigation');
+nav.classList.remove('main-navigation--opened');
+buttonNav.classList.remove('main-navigation__toggle--no-js');
 buttonNav.addEventListener('click', function () {
   nav.classList.toggle('main-navigation--opened');
 });
